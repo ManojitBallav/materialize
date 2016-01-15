3384fbf06524246d8b48455027e236da0ee0fc73
Manojit Ballav <manojitocto@gmail.com>
2016-01-15 23:48:34 +0530

    First commit

---
+++ assets/js/js.js
@@ -0,0 +1,11 @@
+(function($) {
+	$(document).ready(function(){
+
+			$(".button-collapse").sideNav();
+			$('.materialboxed').materialbox();
+			$('ul.tabs').tabs();
+			$('.parallax').parallax();
+			
+			});
+
+}(jQuery));
